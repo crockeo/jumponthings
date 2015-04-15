70e72bee91149b36004192fa4e155fe08501b208
Cerek Hillen <crockeo@gmail.com>
2015-04-15 16:18:29 -0400

    Initial commit.

---
+++ src/game.hpp
@@ -0,0 +1,14 @@
+#ifndef _GAME_HPP_
+#define _GAME_HPP_
+
+//////////////
+// Includes //
+#include <clibgame.hpp>
+
+//////////
+// Code //
+
+// The default game.
+struct Game : public clibgame::ECP { };
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+//////////////
+// Includes //
+#include <clibgame.hpp>
+#include <exception>
+#include <iostream>
+
+#include "game.hpp"
+
+//////////
+// Code //
+
+// Entry point.
+int main(int argc, char** argv) {
+    // Configuring the game.
+    clibgame::EngineConfig cfg;
+
+    cfg.title = "Jump On Things!!!";
+    cfg.ups = 240;
+    cfg.rps = 120;
+
+    // Starting the game.
+    Game game;
+    try {
+        clibgame::startEngine(cfg, game, "res/assetspec.txt");
+    } catch (std::runtime_error& e) {
+        std::cout << "Failed to start game: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
