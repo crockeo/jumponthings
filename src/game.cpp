2db022b73e665042782169c8af43052e50eaa5a9
Cerek Hillen <crockeo@gmail.com>
2015-04-22 17:13:29 -0400

    I guess got some stuff working.

--- src/game.cpp
+++ src/game.cpp
@@ -11,9 +11,9 @@
 // Constructing a new game.
 Game::Game() {
     addEntity("player");
-    getEntity("player").addComponent(new clibgame::CPosition(0, 0, 32, 64));
+    getEntity("player").addComponent(new clibgame::CPosition(0, 0, 128, 256));
     getEntity("player").addComponent(new PlayerController(1200, 4, 0, 0));
-    getEntity("player").addComponent(new TexableRender(TR_TEXTURE, "playersheet", "testshader",
+    getEntity("player").addComponent(new TexableRender(TR_ANIMATION, "player", "testshader",
                                                        "clibgame_position_player",
-                                                       0, 0, 32, 64));
+                                                       0, 0, 128, 256));
 }
