35bca564f9d94fd2a279c77547de3176f7ae00c4
Cerek Hillen <crockeo@gmail.com>
2015-04-29 09:18:41 -0400

    Some more collision stuff.

--- src/collidable.cpp
+++ src/collidable.cpp
@@ -7,6 +7,28 @@
 //////////
 // Code //
 
+// Creating a collidable set with an already-defined vector of Rectangles.
+CollidableSet::CollidableSet(std::vector<Rectangle> collisions) :
+        collisions(collisions) { }
+
+// Creating a collidable set without any default rectangles.
+CollidableSet::CollidableSet() { }
+
+// Getting this set's collision rectangles.
+std::vector<Rectangle> CollidableSet::getCollisionRectangles() const {
+    return collisions;
+}
+
+// Adding a new collision rectangle.
+void CollidableSet::addCollision(Rectangle r) {
+    collisions.push_back(r);
+}
+
+// Getting the name of the component.
+std::string CollidableSet::getName() const {
+    return "collidableSet";
+}
+
 // Getting the collision rectangle from a built-in position component.
 Rectangle getCollisionRectangle(const clibgame::CPosition& pos) {
     return Rectangle(pos.getX(), pos.getY(),
@@ -20,6 +42,12 @@ std::vector<Rectangle> getCollisionRectangles(const clibgame::Entity& caller, co
         const clibgame::Entity& e = ecp.getEntity(std::get<0>(*it));
         if (e.getUID() == caller.getUID())
             continue;
+
+        if (e.hasComponent("collidableSet")) {
+            const CollidableSet& cs = dynamic_cast<const CollidableSet&>(e.getComponent("collidableSet"));
+            for (Rectangle r: cs.getCollisionRectangles())
+                rs.push_back(r);
+        }
 
         if (e.hasComponent("clibgame_position")) {
             const clibgame::CPosition& pos = dynamic_cast<const clibgame::CPosition&>(e.getComponent("clibgame_position"));
--- src/collidable.hpp
+++ src/collidable.hpp
@@ -11,11 +11,35 @@
 //////////
 // Code //
 
+// A set of collisions.
+class CollidableSet : public clibgame::Component {
+private:
+    std::vector<Rectangle> collisions;
+
+public:
+    // Creating a collidable set with an already-defined vector of Rectangles.
+    CollidableSet(std::vector<Rectangle>);
+
+    // Creating a collidable set without any default rectangles.
+    CollidableSet();
+
+    // Getting this set's collision rectangles.
+    std::vector<Rectangle> getCollisionRectangles() const;
+
+    // Adding a new collision rectangle.
+    void addCollision(Rectangle);
+
+    // Getting the name of the component.
+    virtual std::string getName() const override;
+};
+
 // Getting the collision rectangle from a built-in position component.
 Rectangle getCollisionRectangle(const clibgame::CPosition&);
 
 // Getting the set of collision rectangles from the ECP.
 std::vector<Rectangle> getCollisionRectangles(const clibgame::Entity&, const clibgame::ECP&);
+
+// TODO: Scrap below this.
 
 // An event meant to propagate the information regaring a 
 class CollidableEvent : public clibgame::Event {
